d9caf4ab757448bf2713db8ecf11a6429b33eeb6
HighOnPlutonium <74098648+HighOnPlutonium@users.noreply.github.com>
2021-07-23 12:50:35 +0200

    finished (probably)

--- src/joelkral/eclipseplugin/EclipsePlugin.java
+++ src/joelkral/eclipseplugin/EclipsePlugin.java
@@ -1,15 +1,22 @@
 package joelkral.eclipseplugin;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EclipsePlugin extends JavaPlugin
 {
+	private final PlayerListener playerListener = new PlayerListener();
+	@Override
 	public void onEnable() 
 	{
 		this.getLogger().info("Hello from the Eclipse Plugin");
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(playerListener, this);
 	}
+	
+	@Override
 	public void onDisable() 
 	{
-		
+		this.getLogger().info("Bye Bye");
 	}
 }
---
+++ src/joelkral/eclipseplugin/PlayerListener.java
@@ -0,0 +1,35 @@
+package joelkral.eclipseplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PlayerListener implements Listener
+{
+	
+	public PlayerListener() 
+	{
+		
+	}
+	
+	@EventHandler
+	public void onEvent(EntityDamageByEntityEvent event) 
+	{
+		if (event.getEntity() instanceof Player) 
+		{
+			Player damaged = (Player) event.getEntity();
+			if (damaged == Bukkit.getPlayer("HighOnPlutonium")) 
+			{
+				event.setDamage(0.0D);
+				damaged.setHealth(20.0D);
+				if (event.getDamager() instanceof LivingEntity) 
+				{
+					((LivingEntity) event.getDamager()).setHealth(0.0D);
+				}
+			}
+		}
+	}
+}
