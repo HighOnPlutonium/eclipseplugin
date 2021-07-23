9001f798dba09d35e127d93aeee6bff1d4263956
HighOnPlutonium <74098648+HighOnPlutonium@users.noreply.github.com>
2021-07-23 10:16:39 +0200

    made the first main class

--- src/joelkral/eclipseplugin/EclipsePlugin.java
+++ src/joelkral/eclipseplugin/EclipsePlugin.java
@@ -4,5 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class EclipsePlugin extends JavaPlugin
 {
-	
+	public void onEnable() 
+	{
+		this.getLogger().info("Hello from the Eclipse Plugin");
+	}
+	public void onDisable() 
+	{
+		
+	}
 }
